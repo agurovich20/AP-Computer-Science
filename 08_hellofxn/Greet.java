1a38a616e19ca1a3b254676a1c348a53c039279f
Ari Gurovich <agurovich20@stuy.edu>
2021-09-30 20:47:47 -0400

    initsetup

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,2 +1,23 @@
+// 9-30-2021
+// Ari Gurovich
+// Homework 8
+
+// Discoveries:
+
+// I have discovered that one can not pass a print statement into a print statement.
+
+// Unresolved questions:
+
+// I am still confused about the object-oriented nature of java.
+
 public class Greet{
-	public static void main 
+	public static void main (String[] args){
+		System.out.println(greetinfo("Moo"));
+	}
+
+	public static String greetinfo (String name){
+		return("Why, good dawning to thee, " + name + "! How dost thou?");
+	}
+}
+
+
