465bc61c9ad2082847b7f7db070e3518120a4d68
Ari Gurovich <agurovich20@stuy.edu>
2021-10-05 13:34:33 -0400

    initsetup

---
+++ 10_bigsib/greet.java
@@ -0,0 +1,9 @@
+public class Greet {
+        public static void main (String[] args){
+        //      System.out.println("No hablo queso");
+                System.out.println(BigSib.greet("Ziying"));
+                System.out.println(BigSib.greet("Kartic"));
+                System.out.println(BigSib.greet("Ari"));
+        }                               
+
+}
