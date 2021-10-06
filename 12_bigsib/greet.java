63870bfed131957d489eab76ad4f5993020431db
Ari Gurovich <agurovich20@stuy.edu>
2021-10-06 17:19:12 -0400

    initsetup

---
+++ 12_bigsib/greet.java
@@ -0,0 +1,23 @@
+/*
+
+Ari, John, Eric
+
+APCS HW 10
+
+10/5/21
+
+Discovery: return type needs to match output method
+
+
+*/
+
+
+
+public class Greet {
+        public static void main (String[] args){
+                System.out.println(BigSib.greet("Ari"));
+                System.out.println(BigSib.greet("John"));
+                System.out.println(BigSib.greet("Eric"));
+        }
+
+}
