ad5420efafedab3fc83b8095c16a8efe6b32b22a
Ari Gurovich <agurovich20@stuy.edu>
2021-10-24 21:26:27 -0400

    initsetup

---
+++ 23_coin/driver.java
@@ -0,0 +1,45 @@
+/*
+Team Potato Pounders (Human: Justin, Ari; Ducks: Alfred, Javier)
+APCS
+HW23 -- What Does Equality Look Like?
+2021-10-23
+time spent: 0.6 hours
+QCC:
+DISCO:
+*/
+
+public class Driver {
+
+  public static void main( String[] args ) {
+
+    //build Objects from blueprint specified by class Coin
+
+
+    //test default constructor
+      Coin mine = new Coin();
+      //test 1st overloaded constructor
+      Coin yours = new Coin( "quarter" );
+      //test 2nd overloaded constructor
+      Coin wayne = new Coin( "dollar", "heads" );
+      //test toString() methods of each Coin
+      System.out.println("mine: " + mine);
+      System.out.println("yours: " + yours);
+      System.out.println("wayne: " + wayne);
+      //test flip() method
+      System.out.println("\nAfter flipping...");
+      yours.flip();
+      wayne.flip();
+      System.out.println("yours: " + yours);
+      System.out.println("wayne: " + wayne);
+      //test equals() method
+      if ( yours.equals(wayne) ) {
+        System.out.println( "Matchee matchee!" );
+      }
+      else {
+        System.out.println( "No match. Firestarter you can not be." );
+      }
+
+
+  }//end main()
+
+}//end class
